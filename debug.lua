08540be3edb472629c8bcbc74b73d8f023aef31d
nu <windykerenza@gmail.com>
2024-10-26 23:24:45 +0700

    Add files via upload

---
+++ debug.lua
@@ -0,0 +1,22 @@
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+-- stop here don't check my github anymore
+hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function() return end)
+hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function() return end)
+hookfunction(require(game:GetService("ReplicatedStorage"):WaitForChild("GuideModule")).ChangeDisplayedNPC,function() return end)
